d8fb9ce8bbaac79b88e25a8495c405942b6e367a
Saga-sanga <vulcan248@gmail.com>
2023-03-27 21:42:03 +0530

    Feat: Remove old App code add basic content

--- src/App.tsx
+++ src/App.tsx
@@ -7,18 +7,8 @@ const App: Component = () => {
   return (
     <div class={styles.App}>
       <header class={styles.header}>
-        <img src={logo} class={styles.logo} alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          class={styles.link}
-          href="https://github.com/solidjs/solid"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn Solid
-        </a>
+        <h1>Hello! I'm Reckson Khiangte</h1>
+        <h2>Welcome to my site! This is a Work In Progress</h2>
       </header>
     </div>
   );
