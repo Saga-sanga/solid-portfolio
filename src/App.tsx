953832d5cbd2bdeaf1ba26bece7f0fd6819bff18
Saga-sanga <vulcan248@gmail.com>
2023-03-30 21:23:40 +0530

    Feat: Add socials link

--- src/App.tsx
+++ src/App.tsx
@@ -6,11 +6,19 @@ import styles from './App.module.css';
 const App: Component = () => {
   return (
     <div class={styles.App}>
-      <header class='min-h-screen flex flex-col items-center justify-center gap-8'>
+      <section class='min-h-screen flex flex-col items-center justify-center gap-8'>
         <h1 class='text-5xl text-semibold'>Hello! I'm Reckson Khiangte</h1>
         <h2 class='text-3xl'>Welcome to my site!</h2>
         <p>This is a Work In Progress...</p>
-      </header>
+        <article>
+          {/* TODO: Add bookmarked socials style from Twitter & Socials Icons*/}
+          <ul class='flex gap-2'>
+            <li class='hover:text-white'><a href="https://github.com/Saga-sanga" target='_blank'>Github</a></li>
+            <li class='hover:text-white'><a href="https://twitter.com/RecksonKhiangte" target='_blank'>Twitter</a></li>
+            <li class='hover:text-white'><a href="https://www.linkedin.com/in/reckson-khiangte-65a2a3190/" target='_blank'>Linkedin</a></li>
+          </ul>
+        </article>
+      </section>
     </div>
   );
 };
