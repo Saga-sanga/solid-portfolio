a29424a8f41427154c262d0241ec3543417f0493
Saga-sanga <vulcan248@gmail.com>
2023-04-14 13:31:21 +0530

    Feat: Add social media icons and daisy theme setup

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,9 @@ import type { Component } from 'solid-js';
 import profile from './assets/profile.jpeg'
 import { NavBar } from './components/NavBar';
 
-import devLogo from './assets/dev-logo.svg';
+import github from './assets/github.svg'
+import linkedin from './assets/linkedin_logo_icon.svg';
+import twitter from './assets/twitter_logo_icon.svg'
 import styles from './App.module.css';
 
 const App: Component = () => {
@@ -10,16 +12,16 @@ const App: Component = () => {
     <div class={styles.App}>
       <NavBar/>
       <section class='min-h-full mt-12 flex flex-col items-center justify-center gap-8'>
-        <img class='mask mask-circle border-8 border-accent rounded-full' src={profile} alt="profile image" width={250} height={250}/>
-        <h1 class='text-5xl text-semibold'>Hello! I'm Reckson Khiangte</h1>
-        <h2 class='text-3xl'>Web Developer</h2>
-        <p>This is a Work In Progress...</p>
+        <img class='mask mask-circle border-8 border-primary rounded-full' src={profile} alt="profile image" width={250} height={250}/>
+        <h1 class='text-5xl font-bold'>Hello! I'm Reckson Khiangte 👋</h1>
+        <h2 class='text-3xl max-w-xl'>I'm a <b>Web Developer</b> who's currently learning and transitioning into <b>Web3</b> with all its <b>Blockchain</b> goodness.</h2>
+        <p>🚧 This is a Work In Progress... 🚧</p>
         <article>
           {/* TODO: Add bookmarked socials style from Twitter & Socials Icons*/}
-          <ul class='flex gap-2'>
-            <li class='hover:opacity-60'><a href="https://github.com/Saga-sanga" target='_blank'>Github</a></li>
-            <li class='hover:opacity-60'><a href="https://twitter.com/RecksonKhiangte" target='_blank'>Twitter</a></li>
-            <li class='hover:opacity-60'><a href="https://www.linkedin.com/in/reckson-khiangte-65a2a3190/" target='_blank'>Linkedin</a></li>
+          <ul class='flex gap-6'>
+            <li class='hover:opacity-60'><a href="https://github.com/Saga-sanga" target='_blank'><img src={github} alt="github" width={30} height={30}/></a></li>
+            <li class='hover:opacity-60'><a href="https://twitter.com/RecksonKhiangte" target='_blank'><img src={twitter} alt="twitter" width={30} height={30}/></a></li>
+            <li class='hover:opacity-60'><a href="https://www.linkedin.com/in/reckson-khiangte-65a2a3190/" target='_blank'><img src={linkedin} alt="linkedin" width={30} height={30}/></a></li>
           </ul>
         </article>
       </section>
