09163d552deb61eaff070cec664fafb9840be1af
Saga-sanga <vulcan248@gmail.com>
2023-04-14 20:42:40 +0530

    Content: Small tweaks to hero text

--- src/App.tsx
+++ src/App.tsx
@@ -12,12 +12,12 @@ const App: Component = () => {
     <div class={styles.App}>
       <NavBar/>
       <section class='min-h-full mt-12 flex flex-col items-center justify-center gap-8'>
-        <img class='mask mask-circle border-8 border-primary rounded-full' src={profile} alt="profile image" width={250} height={250}/>
+        <img class='mask mask-circle border-4 border-primary rounded-full' src={profile} alt="profile image" width={250} height={250}/>
         <h1 class='text-5xl font-bold'>Hello! I'm Reckson Khiangte 👋</h1>
-        <h2 class='text-3xl max-w-xl'>I'm a <b>Web Developer</b> who's currently learning and transitioning into <b>Web3</b>. Delving into the world of <b>Blockchain</b> with all it's cutting edge goodness.</h2>
+        <h2 class='text-3xl max-w-xl'>I'm a <b>Web Developer</b> who's transitioning into <b>Web3</b>; Currently delving into the exciting world of <b>Blockchain</b> with all it's cutting edge goodness.</h2>
         <p>🚧 This site is a Work In Progress... 🚧</p>
         <article>
-          {/* TODO: Add bookmarked socials style from Twitter & Socials Icons*/}
+          {/* TODO: Add inline SVG and set fill color to currentcolor to change color on different states */}
           <ul class='flex gap-6'>
             <li class='hover:opacity-60'><a href="https://github.com/Saga-sanga" target='_blank'><img src={github} alt="github" width={30} height={30}/></a></li>
             <li class='hover:opacity-60'><a href="https://twitter.com/RecksonKhiangte" target='_blank'><img src={twitter} alt="twitter" width={30} height={30}/></a></li>
