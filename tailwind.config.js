a2bed1fc7c291d718061619b3038b2234c6f5ef3
Saga-sanga <vulcan248@gmail.com>
2023-05-08 20:29:31 +0530

    Chore: Change dark theme from 'Forest' to 'Dracula'

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,21 +1,18 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [
-    "./index.html",
-    "./src/**/*.{js,jsx,ts,tsx}",
-  ],
+  content: ["./index.html", "./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
     extend: {},
   },
   plugins: [require("daisyui")],
   daisyui: {
     styled: true,
-    themes: ['cupcake', 'forest'],
+    themes: ["cupcake", "dracula"],
     base: true,
     utils: true,
     logs: true,
     rtl: false,
     prefix: "",
-    darkTheme: "forest",
+    darkTheme: "dracula",
   },
-}
+};
