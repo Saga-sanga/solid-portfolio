0ac4c885bce2671f5ed8b48d8bf1bc0d6ebcfc8e
Saga-sanga <vulcan248@gmail.com>
2023-05-08 14:42:10 +0530

    Feat: Add tech stack

--- src/App.tsx
+++ src/App.tsx
@@ -5,49 +5,55 @@ import github from "./assets/github.svg";
 import linkedin from "./assets/linkedin_logo_icon.svg";
 import twitter from "./assets/twitter_logo_icon.svg";
 import styles from "./App.module.css";
+import TechList from "./components/TechList";
 
 function App() {
   return (
     <div class={styles.App}>
       <NavBar />
-      <section class="min-h-full mt-12 mx-4 flex flex-col items-center justify-center gap-8">
-        <img
-          class="mask mask-circle border-4 border-primary rounded-full"
-          src={profile}
-          alt="profile image"
-          width={250}
-          height={250}
-        />
-        <h1 class="text-5xl font-bold">Hello! I'm Reckson Khiangte 👋</h1>
-        <h2 class="text-2xl max-w-xl">
-          I'm a <b>Web Developer</b> who's transitioning into <b>Web3</b>;
-          Currently delving into the exciting world of <b>Blockchain</b> with
-          all it's cutting edge goodness.
-        </h2>
-        <p>🚧 This site is a Work In Progress... 🚧</p>
-        <article>
-          <ul class="flex gap-6 mb-12">
-            <li class="hover:scale-110 ease-in duration-200">
-              <a href="https://github.com/Saga-sanga" target="_blank">
-                <img src={github} alt="github" width={30} height={30} />
-              </a>
-            </li>
-            <li class="hover:scale-110 ease-in duration-200">
-              <a href="https://twitter.com/RecksonKhiangte" target="_blank">
-                <img src={twitter} alt="twitter" width={30} height={30} />
-              </a>
-            </li>
-            <li class="hover:scale-110 ease-in duration-200">
-              <a
-                href="https://www.linkedin.com/in/reckson-khiangte-65a2a3190/"
-                target="_blank"
-              >
-                <img src={linkedin} alt="linkedin" width={30} height={30} />
-              </a>
-            </li>
-          </ul>
+      <main class="min-h-full mt-12 mx-auto flex flex-col items-center justify-center gap-8 max-w-4xl">
+        <article class="flex flex-col-reverse items-center md:flex-row gap-10 mx-6">
+          <div class="flex flex-col items-center md:items-start gap-8 max-w-md">
+            <h1 class="text-5xl font-bold md:text-left">
+              Hello! I'm Reckson Khiangte 👋
+            </h1>
+            <h2 class="text-2xl max-w-xl">
+              I'm a <b>Front-end Developer</b> from India 🇮🇳
+            </h2>
+            <article>
+              <ul class="flex gap-6">
+                <li class="hover:scale-110 ease-in duration-200">
+                  <a href="https://github.com/Saga-sanga" target="_blank">
+                    <img src={github} alt="github" width={30} height={30} />
+                  </a>
+                </li>
+                <li class="hover:scale-110 ease-in duration-200">
+                  <a href="https://twitter.com/RecksonKhiangte" target="_blank">
+                    <img src={twitter} alt="twitter" width={30} height={30} />
+                  </a>
+                </li>
+                <li class="hover:scale-110 ease-in duration-200">
+                  <a
+                    href="https://www.linkedin.com/in/reckson-khiangte-65a2a3190/"
+                    target="_blank"
+                  >
+                    <img src={linkedin} alt="linkedin" width={30} height={30} />
+                  </a>
+                </li>
+              </ul>
+            </article>
+          </div>
+          <img
+            class="mask mask-circle border-4 border-primary rounded-full"
+            src={profile}
+            alt="profile image"
+            width={250}
+            height={250}
+          />
         </article>
-      </section>
+        <hr class="w-1/2" />
+        <TechList />
+      </main>
     </div>
   );
 }
---
+++ src/components/TechList.tsx
@@ -0,0 +1,44 @@
+import html5 from "../assets/html5.svg";
+import css3 from "../assets/css-3.svg";
+import javascript from "../assets/javascript.svg";
+import typescript from "../assets/typescript.svg";
+import reactLogo from "../assets/react.svg";
+import tailwindCss from "../assets/tailwind-css.svg";
+import nextjs from "../assets/nextjs.svg";
+
+function TechList() {
+  return (
+    <div>
+      <ul class="flex flex-wrap justify-center gap-4">
+        <li class="flex items-center justify-center rounded-full h-16 w-16 shadow">
+          <img width={32} height={32} src={html5} alt="HTML logo" />
+        </li>
+        <li class="flex items-center justify-center rounded-full h-16 w-16 shadow">
+          <img width={32} height={32} src={css3} alt="CSS logo" />
+        </li>
+        <li class="flex items-center justify-center rounded-full h-16 w-16 shadow">
+          <img width={32} height={32} src={javascript} alt="Javascript logo" />
+        </li>
+        <li class="flex items-center justify-center rounded-full h-16 w-16 shadow">
+          <img width={32} height={32} src={typescript} alt="Typescript logo" />
+        </li>
+        <li class="flex items-center justify-center rounded-full h-16 w-16 shadow">
+          <img width={32} height={32} src={reactLogo} alt="React logo" />
+        </li>
+        <li class="flex items-center justify-center rounded-full h-16 w-16 shadow">
+          <img width={32} height={32} src={nextjs} alt="NextJs logo" />
+        </li>
+        <li class="flex items-center justify-center rounded-full h-16 w-16 shadow">
+          <img
+            width={32}
+            height={32}
+            src={tailwindCss}
+            alt="Tailwind CSS logo"
+          />
+        </li>
+      </ul>
+    </div>
+  );
+}
+
+export default TechList;
